d52be21104bc23ae4587f38b9c68db55ef27ddb5
Levin-Andrey <lennytmp@gmail.com>
2015-06-29 00:08:00 +0100

    memory allocation for agents

--- code/main.cpp
+++ code/main.cpp
@@ -5,9 +5,41 @@ void* allocateMemory(int memorySize) {
 	return VirtualAlloc(NULL, memorySize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 }
 
+struct MemoryBlock {
+	void* base;
+	int size;
+};
+
+struct AgentIn {
+	MemoryBlock workMemory;
+	void* updateFn;
+};
+
+void simulate(MemoryBlock gameMemory, AgentIn* agents, int agentsCount) {
+	int a = 1;
+}
+
+#define DEBUG_AGENT_COUNT 8
+#define KILOBYTES(Value) ((Value)*1024LL)
+#define MEGABYTES(Value) (KILOBYTES(Value)*1024LL)
+#define GIGABYTES(Value) (MEGABYTES(Value)*1024LL)
+#define TERABYTES(Value) (GIGABYTES(Value)*1024LL)
+
 int main() {
+	MemoryBlock gameMemory;
+	gameMemory.size = MEGABYTES(100);
+	gameMemory.base = allocateMemory(gameMemory.size);
+	AgentIn agents[DEBUG_AGENT_COUNT];
+	for (int i = 0; i < DEBUG_AGENT_COUNT; i++) {
+		agents[i].workMemory.size = MEGABYTES(1);
+		agents[i].workMemory.base = allocateMemory(agents[i].workMemory.size);
+	}
+	simulate(gameMemory, agents, DEBUG_AGENT_COUNT);
+
+	char fileName[MAX_PATH];
 	HMODULE currentModule = GetModuleHandle(NULL);
-	void* memoryStart = allocateMemory(1024 * 1024 * 1024);
+	GetModuleFileName(currentModule, fileName, MAX_PATH);
+	
 	printf("Hello world!");
 	return 42;
 }
