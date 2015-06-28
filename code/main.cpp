abb1f327385eb717eb35a7ecb7f5af22a53a3740
Levin-Andrey <lennytmp@gmail.com>
2015-06-28 23:43:14 +0100

    allocating memory for windows

--- code/main.cpp
+++ code/main.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <Windows.h>
+
+void* allocateMemory(int memorySize) {
+	return VirtualAlloc(NULL, memorySize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+}
 
 int main() {
+	HMODULE currentModule = GetModuleHandle(NULL);
+	void* memoryStart = allocateMemory(1024 * 1024 * 1024);
 	printf("Hello world!");
 	return 42;
 }
